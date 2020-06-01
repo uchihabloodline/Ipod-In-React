092a1eea74c3875c7086ec351a0053bcd2358d35
shivam <geetapandey595@gmail.com>
2020-06-02 03:26:52 +0530

    Ipod project done uploded

---
+++ src/screen.js
@@ -0,0 +1,29 @@
+// importing required files
+import React from'react';
+import './ipod.css'
+import ZingTouch from 'zingtouch';
+
+const Screen = (props) => {
+    return(
+        <div>
+
+            <div unselectable="off" className="screen" id="display">
+                {
+                    props.dispItems.map((item,index) =>(
+                        <div className="screen-elements" id={item} key={index}>
+                            {item}
+                        </div>
+                    ))
+                }
+            </div>
+            <div id="Coverflow-screen" className="newScreen"></div>
+            <div id="Music-screen" className="newScreen"></div>
+            <div id="Games-screen" className="newScreen"></div>
+            <div id="Settings-screen" className="newScreen"></div>
+
+        </div>
+        
+    );
+}
+//Exporting Screen
+export default Screen;
